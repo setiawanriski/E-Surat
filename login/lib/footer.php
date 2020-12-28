2c0bca8b2d010ba9b1b093874cbdcd50cee6cb4a
setiawanar <57240935+setiawanar@users.noreply.github.com>
2020-12-28 12:00:49 +0700

    WIP

---
+++ login/lib/footer.php
@@ -0,0 +1,9 @@
+<div class="footer" style="position: fixed;bottom: 0%;" >
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+                             Copyright © 2020 <a href="https://instagram.com/langkah_menjauh">Setiawan AR</a>.
+                        </div>
+                    </div>
+                </div>
+            </div>
---
+++ login/lib/sidebar.php
@@ -0,0 +1,73 @@
+<div class="nav-left-sidebar sidebar-dark">
+            <div class="menu-list">
+                <nav class="navbar navbar-expand-lg navbar-light">
+                    <a class="d-xl-none d-lg-none" href="#">Dashboard</a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav flex-column">
+                            <li class="nav-divider">
+                                Menu
+                            </li>
+                            <li class="nav-item ">
+                                <a class="nav-link active" href="#" data-toggle="collapse" aria-expanded="false" data-target="#submenu-1" aria-controls="submenu-1"><i class="fa fa-fw fa-user-circle"></i>Dashboard <span class="badge badge-success">6</span></a>
+                                <div id="submenu-1" class="collapse submenu" style="">
+                                    <ul class="nav flex-column">
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="dashboard">Dasboard</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#" data-toggle="collapse" aria-expanded="false" data-target="#submenu-1-2" aria-controls="submenu-1-2">Data User</a>
+                                            <div id="submenu-1-2" class="collapse submenu" style="">
+                                                <ul class="nav flex-column">
+                                                    <li class="nav-item">
+                                                        <a class="nav-link" href="list-user">List user</a>
+                                                    </li>
+                                                    <li class="nav-item">
+                                                        <a class="nav-link" href="tambah-user">Tambah user</a>
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#" data-toggle="collapse" aria-expanded="false" data-target="#submenu-3-4-5" aria-controls="submenu-1-2">Data Surat</a>
+                                            <div id="submenu-3-4-5" class="collapse submenu" style="">
+                                                <ul class="nav flex-column">
+                                                    <li class="nav-item">
+                                                        <a class="nav-link" href="surat-masuk">Surat masuk</a>
+                                                    </li>
+                                                    <li class="nav-item">
+                                                        <a class="nav-link" href="surat-keluar">Surat keluar</a>
+                                                    </li>
+                                                    <li class="nav-item">
+                                                        <a class="nav-link" href="jenis-surat">Jenis keluar</a>
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#" data-toggle="collapse" aria-expanded="false" data-target="#submenu-6-7" aria-controls="submenu-1-2">Laporan Surat</a>
+                                            <div id="submenu-6-7" class="collapse submenu" style="">
+                                                <ul class="nav flex-column">
+                                                    <li class="nav-item">
+                                                        <a class="nav-link" href="laporan-surat-masuk">Laporan surat masuk</a>
+                                                    </li>
+                                                    <li class="nav-item">
+                                                        <a class="nav-link" href="laporan-surat-keluar">Laporan surat keluar</a>
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="logout">Logout</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </li>
+                            
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </div>
